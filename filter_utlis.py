f6eb65f0c4b3bb304affb2fab1b63e1d7db9c57c
lore <lorenzosaavedra.c@gmail.com>
2025-07-26 14:53:30 -0400

    filtered stimulus for large and small diagram grasp

---
+++ filter_utlis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def filter_dataframe(data, exercises):
+    emg_signal = pd.DataFrame(data["emg"])
+    stimulus_signal = pd.DataFrame(data["stimulus"])
+    subject = pd.DataFrame(data["subject"])
+
+    df = pd.DataFrame(emg_signal)
+    df["stimulus"] = stimulus_signal
+    df["subject"] = subject
+
+    filtered_df = df[df["stimulus"].isin(exercises)].copy()
+    last_nonzero_index = filtered_df[filtered_df["stimulus"] != 0].index[-1]
+    # Assuming 'filtered_df' is your DataFrame and 'last_nonzero_index' holds the index
+    trimmed_df = filtered_df.loc[: last_nonzero_index + 7000].copy()
+    return trimmed_df
