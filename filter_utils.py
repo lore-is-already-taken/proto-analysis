5a8e038c635d836dbaf0bfa1ee501fae37f7ef59
lore <lorenzosaavedra.c@gmail.com>
2025-07-27 01:05:23 -0400

    fixed typo in filenames

---
+++ filter_utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def filter_dataframe(data, exercises):
+    emg_signal = pd.DataFrame(data["emg"])
+    stimulus_signal = pd.DataFrame(data["stimulus"])
+    subject = pd.DataFrame(data["subject"])
+
+    df = pd.DataFrame(emg_signal)
+    df["stimulus"] = stimulus_signal
+    df["subject"] = subject
+
+    trimmed_df = filter_exercise(df,exercises)
+    return trimmed_df
+
+
+def filter_exercise(df, exercises):
+    filtered_df = df[df["stimulus"].isin(exercises)].copy()
+    last_nonzero_index = filtered_df[filtered_df["stimulus"] != 0].index[-1]
+    print(last_nonzero_index)
+    # Assuming 'filtered_df' is your DataFrame and 'last_nonzero_index' holds the index
+    trimmed_df = filtered_df.loc[:last_nonzero_index + 7000].copy()
+    return trimmed_df
+
